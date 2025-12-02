784710460e314b44a4a0d4dafe5faee9ef4f2cd1
Toavina051 <hei.toavina.2@gmail.com>
2025-12-02 08:52:51 +0300

    fix:change database name Capital letter to lowercase

--- JDBC/src/main/java/TestConnection.java
+++ JDBC/src/main/java/TestConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class TestConnection {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/JDBC";
+        String url = "jdbc:postgresql://localhost:5432/jdbc";
         String user = "postgres";
         String password = "*******";
 
